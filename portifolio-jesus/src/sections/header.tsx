254a5e082bb56ec129de42e8775e7d52e6dbc90d
JuniorPastorini <juniorpastorini_@hotmail.com>
2024-06-20 15:32:35 -0300

    V-1.0.0

--- portifolio-jesus/src/sections/header.tsx
+++ portifolio-jesus/src/sections/header.tsx
@@ -25,7 +25,7 @@ export function Header() {
             path: "#certifications",
         },
     ];
-
+    //ajuste
     return (
         <nav className="fixed mx-auto top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
